c32f7993b10b82abdb810fd8a10c568723441f21
nembo93 <francesco.c193@libero.it>
2022-10-26 16:13:07 +0200

    Aggiunge funzione per inserire immagine da un array in un elemento del dom

--- js/main.js
+++ js/main.js
@@ -2,21 +2,27 @@
 // DICHIARARE UN ARRAY CON ALL'INTERNO L'INDIRIZZO DELL'IMMAGINE
 // const immagini = document.querySelectorAll("img");
 // console.log(immagini);
-const immaggini = [
+const immagini = [
     `img/img/01.jpg`,
     `img/img/02.jpg`,
     `img/img/03.jpg`,
     `img/img/04.jpg`,
     `img/img/05.jpg`,
 ];
-console.log(immaggini);
+console.log(immagini);
 
 
-// PER OGNI ELEMENTO DELLA LISTA CREARE UN DIV E INSERIRCI L'IMMAGINE
-for (i = 0; i < immaggini.length; i++){
+// PER OGNI ELEMENTO DELLA LISTA CREARE UN DIV ALL'INTERNO DI MAIN_BOX A CUI AGGIUNGERE UNA CLASSE E INSERIRCI L'IMMAGINE
+for (i = 0; i < immagini.length; i++){
     let item = document.createElement(`div`);
     item.classList.add(`item`);
     document.getElementById(`main_box`).appendChild(item);
+
+    // Inserire l?immagine nel div con classe item
+    let jsImg = document.createElement(`img`);
+    jsImg.src = immagini[i];
+    document.getElementsByClassName(`item`).appendChild(jsImg);
+    // document.getElementsByClassName(`item`).innerHTML(immagini[i]);
 }
 
 
